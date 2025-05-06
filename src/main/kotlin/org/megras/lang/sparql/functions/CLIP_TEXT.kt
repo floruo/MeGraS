3c79e8639d2da433f104ed377567a20160ab40e3
floruo <florian.ruosch@uzh.ch>
2025-05-06 11:44:08 +0200

    Initial working version of the CLIP_TEXT function

--- src/main/kotlin/org/megras/lang/sparql/functions/CLIP_TEXT.kt
+++ src/main/kotlin/org/megras/lang/sparql/functions/CLIP_TEXT.kt
@@ -2,26 +2,39 @@ package org.megras.lang.sparql.functions
 
 import org.apache.jena.sparql.expr.NodeValue
 import org.apache.jena.sparql.function.FunctionBase1
+import ai.djl.repository.zoo.Criteria
+import ai.djl.repository.zoo.ZooModel
+import ai.djl.inference.Predictor
+import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory
 
 class CLIP_TEXT : FunctionBase1() {
 
     private fun calculateTextEmbedding(text: String): FloatArray {
-        //TODO: Implement the actual embedding calculation
-        // For now, return a dummy embedding of 512 zeros
-        return FloatArray(512) { 0.0f }
+        val criteria: Criteria<String, FloatArray> = Criteria.builder()
+            .setTypes(String::class.java, FloatArray::class.java)
+            .optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/clip-ViT-B-32-multilingual-v1")
+            .optTranslatorFactory(TextEmbeddingTranslatorFactory())
+            .optEngine("PyTorch")
+            .build()
+
+        try {
+            criteria.loadModel().use { model: ZooModel<String, FloatArray> ->
+                model.newPredictor().use { predictor: Predictor<String, FloatArray> ->
+                    val embeddings: FloatArray = predictor.predict(text)
+                    return embeddings
+                }
+            }
+        } catch (e: Exception) {
+            throw Exception("Error calculating text embedding: ${e.message}")
+        }
     }
 
     override fun exec(arg: NodeValue): NodeValue {
         val text = arg.asString()
 
-        try {
-            val embeddingResult = calculateTextEmbedding(text)
-
-            val embeddingString = embeddingResult.joinToString(",")
-            return NodeValue.makeString(embeddingString)
-
-        } catch (e: Exception) {
-            throw Exception("Error calculating text embedding: ${e.message}")
-        }
+        val embeddingResult = calculateTextEmbedding(text)
+        //TODO : Convert the FloatArray to a format suitable for NodeValue
+        val embeddingString = embeddingResult.joinToString(",")
+        return NodeValue.makeString(embeddingString)
     }
 }
