f30a06e816f7a7456e62501563fb93fa43329849
floruo <florian.ruosch@uzh.ch>
2025-05-07 15:18:25 +0200

    Clean up imports in JenaGraphWrapper

--- src/main/kotlin/org/megras/lang/sparql/jena/JenaGraphWrapper.kt
+++ src/main/kotlin/org/megras/lang/sparql/jena/JenaGraphWrapper.kt
@@ -1,13 +1,9 @@
 package org.megras.lang.sparql.jena
 
-import org.apache.jena.graph.Node
 import org.apache.jena.graph.Triple
 import org.apache.jena.graph.impl.GraphBase
 import org.apache.jena.util.iterator.ExtendedIterator
-import org.megras.data.graph.QuadValue
-import org.megras.data.graph.URIValue
 import org.megras.graphstore.QuadSet
-import org.megras.lang.sparql.SparqlUtil
 import org.megras.lang.sparql.SparqlUtil.toQuadValue
 
 class JenaGraphWrapper(private val quads: QuadSet) : GraphBase() {
