c5354b4af6cb3fc8d6ad4e5b9c0d6f7ce7d7eb84
floruo <florian.ruosch@uzh.ch>
2025-05-09 14:37:38 +0200

    Changed from redirect to result for the SPARQL UI #23

--- src/main/kotlin/org/megras/api/rest/handlers/SparqlUiHandler.kt
+++ src/main/kotlin/org/megras/api/rest/handlers/SparqlUiHandler.kt
@@ -1,10 +1,20 @@
 package org.megras.api.rest.handlers
 
+import io.javalin.http.ContentType
 import io.javalin.http.Context
 import org.megras.api.rest.GetRequestHandler
 
+
 class SparqlUiHandler : GetRequestHandler {
     override fun get(ctx: Context) {
-        ctx.redirect("/Snorql-UI/index.html")
+        // Read the HTML file from resources
+        val htmlFile = this::class.java.getResource("/public/Snorql-UI/index.html")
+            ?: throw IllegalStateException("HTML file not found in resources")
+        // Read the content of the HTML file
+        val htmlContent = htmlFile.readText()
+        // Set the content type to HTML
+        ctx.contentType(ContentType.TEXT_HTML.mimeType)
+        // Write the HTML content to the response
+        ctx.result(htmlContent)
     }
 }
