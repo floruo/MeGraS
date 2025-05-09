de884600d7dfe24aac5af90038ebcd2aca08b874
floruo <florian.ruosch@uzh.ch>
2025-05-09 17:51:16 +0200

    Changed COSINE_SIM to only take two vectors as arguments #19

--- src/main/kotlin/org/megras/lang/sparql/functions/CosineSimFunction.kt
+++ src/main/kotlin/org/megras/lang/sparql/functions/CosineSimFunction.kt
@@ -1,10 +1,10 @@
 package org.megras.lang.sparql.functions
 
 import org.apache.jena.sparql.expr.NodeValue
-import org.apache.jena.sparql.function.FunctionBase3
+import org.apache.jena.sparql.function.FunctionBase2
 import org.megras.util.knn.CosineDistance
 
-class CosineSimFunction : FunctionBase3() {
+class CosineSimFunction : FunctionBase2() {
 
     fun parseVector(vectorStr: String): FloatArray {
         /*
@@ -18,14 +18,13 @@ class CosineSimFunction : FunctionBase3() {
         return cleanStr.split(",").map { it.toFloat() }.toFloatArray()
     }
 
-    override fun exec(arg1: NodeValue, arg2: NodeValue, arg3: NodeValue): NodeValue {
-        // split the string representation of the vectors into an array of floats, disregarding the brackets and suffix after ]
-        // e.g. "[1.0, 2.0, 3.0]^^FloatVector" -> [1.0, 2.0, 3.0]
+    override fun exec(arg1: NodeValue, arg2: NodeValue): NodeValue {
+        // Parse the string representation of the vectors into arrays of floats
         val vec1 = parseVector(arg1.asNode().literal.value.toString())
         val vec2 = parseVector(arg2.asNode().literal.value.toString())
-        val threshold = arg3.asNode().literal.value.toString().toDouble()
 
+        // Calculate cosine similarity
         val similarity = 1.0 - CosineDistance.distance(vec1, vec2)
-        return NodeValue.makeBoolean(similarity >= threshold)
+        return NodeValue.makeDouble(similarity)
     }
 }
