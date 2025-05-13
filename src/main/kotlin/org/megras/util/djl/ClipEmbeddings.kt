e9c99d087c9fa47a58cb86dec46f15c604cb49bb
floruo <florian.ruosch@uzh.ch>
2025-05-13 11:02:53 +0200

    Added a CLIP image embedding from path function

--- src/main/kotlin/org/megras/util/djl/ClipEmbeddings.kt
+++ src/main/kotlin/org/megras/util/djl/ClipEmbeddings.kt
@@ -1,7 +1,8 @@
 package org.megras.util.djl
 
-import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory
 import ai.djl.inference.Predictor
+import ai.djl.modality.cv.Image
+import ai.djl.modality.cv.ImageFactory
 import ai.djl.repository.zoo.Criteria
 import ai.djl.repository.zoo.ZooModel
 import kotlin.FloatArray
@@ -14,7 +15,6 @@ class ClipEmbeddings {
             val criteria: Criteria<String, FloatArray> = Criteria.builder()
                 .setTypes(String::class.java, FloatArray::class.java)
                 .optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/clip-ViT-B-32-multilingual-v1")
-                .optTranslatorFactory(TextEmbeddingTranslatorFactory())
                 .optEngine("PyTorch")
                 .build()
 
@@ -27,6 +27,26 @@ class ClipEmbeddings {
                 }
             } catch (e: Exception) {
                 throw Exception("Error calculating text embedding: ${e.message}")
+            }
+        }
+        
+        fun getImageEmbedding(imagePath: String): FloatArray {
+            val criteria: Criteria<Image, FloatArray> = Criteria.builder()
+                .setTypes(Image::class.java, FloatArray::class.java)
+                .optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/clip-ViT-B-32-multilingual-v1")
+                .optEngine("PyTorch")
+                .build()
+
+            try {
+                val image: Image = ImageFactory.getInstance().fromFile(java.nio.file.Paths.get(imagePath))
+                criteria.loadModel().use { model: ZooModel<Image, FloatArray> ->
+                    model.newPredictor().use { predictor: Predictor<Image, FloatArray> ->
+                        val embeddings: FloatArray = predictor.predict(image)
+                        return embeddings
+                    }
+                }
+            } catch (e: Exception) {
+                throw Exception("Error calculating image embedding: ${e.message}")
             }
         }
     }
