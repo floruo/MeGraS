f9103d6a68239d623501704dcefd365a561647e5
floruo <florian.ruosch@uzh.ch>
2025-05-13 16:25:18 +0200

    Aligned registration of derived relations #11

--- src/main/kotlin/org/megras/MeGraS.kt
+++ src/main/kotlin/org/megras/MeGraS.kt
@@ -9,7 +9,7 @@ import org.megras.graphstore.HybridMutableQuadSet
 import org.megras.graphstore.TSVMutableQuadSet
 import org.megras.graphstore.db.PostgresStore
 import org.megras.graphstore.derived.DerivedRelationMutableQuadSet
-import org.megras.graphstore.derived.handlers.AverageColorHandler
+import org.megras.graphstore.derived.DerivedRelationRegistrar
 import org.megras.graphstore.implicit.ImplicitRelationMutableQuadSet
 import org.megras.graphstore.implicit.ImplicitRelationRegistrar
 import org.megras.lang.sparql.FunctionRegistrar
@@ -88,9 +88,8 @@ object MeGraS {
 
         val implicitRelationRegistrar = ImplicitRelationRegistrar()
         quadSet = ImplicitRelationMutableQuadSet(quadSet, implicitRelationRegistrar.getHandlers())
-        quadSet = DerivedRelationMutableQuadSet(quadSet, listOf(
-            AverageColorHandler(quadSet, objectStore)
-        ))
+        val derivedRelationRegistrar = DerivedRelationRegistrar(quadSet, objectStore)
+        quadSet = DerivedRelationMutableQuadSet(quadSet, derivedRelationRegistrar.getHandlers())
 
         RestApi.init(config, objectStore, quadSet)
 
---
+++ src/main/kotlin/org/megras/graphstore/derived/DerivedRelationRegistrar.kt
@@ -0,0 +1,22 @@
+package org.megras.graphstore.derived
+
+import org.megras.data.fs.FileSystemObjectStore
+import org.megras.data.graph.QuadValue
+import org.megras.graphstore.QuadSet
+import org.megras.graphstore.derived.handlers.AverageColorHandler
+
+class DerivedRelationRegistrar(private val quads: QuadSet, private val objectStore: FileSystemObjectStore) {
+    private val handlers = mutableListOf<DerivedRelationHandler<QuadValue>>()
+
+    init {
+        register(AverageColorHandler(quads, objectStore) as DerivedRelationHandler<QuadValue>)
+    }
+
+    private fun register(handler: DerivedRelationHandler<QuadValue>) {
+        handlers.add(handler)
+    }
+
+    fun getHandlers(): List<DerivedRelationHandler<QuadValue>> {
+        return handlers
+    }
+}
