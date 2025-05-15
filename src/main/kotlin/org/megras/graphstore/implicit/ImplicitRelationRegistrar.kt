953e7c80a5be1bcec187ffaa50f08092845c66e7
floruo <florian.ruosch@uzh.ch>
2025-05-15 13:43:50 +0200

    Implemented Temporal Relations #40

--- src/main/kotlin/org/megras/graphstore/implicit/ImplicitRelationRegistrar.kt
+++ src/main/kotlin/org/megras/graphstore/implicit/ImplicitRelationRegistrar.kt
@@ -3,7 +3,13 @@ package org.megras.graphstore.implicit
 import org.megras.graphstore.implicit.handlers.NearDuplicateHandler
 import org.megras.graphstore.implicit.handlers.SamePrefixHandler
 import org.megras.graphstore.implicit.handlers.temporal.AfterHandler
+import org.megras.graphstore.implicit.handlers.temporal.ContainsHandler
+import org.megras.graphstore.implicit.handlers.temporal.EqualsHandler
+import org.megras.graphstore.implicit.handlers.temporal.FinishesHandler
+import org.megras.graphstore.implicit.handlers.temporal.MeetsHandler
+import org.megras.graphstore.implicit.handlers.temporal.OverlapsHandler
 import org.megras.graphstore.implicit.handlers.temporal.PrecedesHandler
+import org.megras.graphstore.implicit.handlers.temporal.StartsHandler
 
 class ImplicitRelationRegistrar {
     private val handlers = mutableListOf<ImplicitRelationHandler>()
@@ -13,6 +19,12 @@ class ImplicitRelationRegistrar {
         //register(NearDuplicateHandler())
         register(AfterHandler())
         register(PrecedesHandler())
+        register(FinishesHandler())
+        register(StartsHandler())
+        register(MeetsHandler())
+        register(ContainsHandler())
+        register(EqualsHandler())
+        register(OverlapsHandler())
     }
 
     private fun register(handler: ImplicitRelationHandler) {
