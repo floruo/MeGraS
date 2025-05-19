c071358acc6ffec7695b129cc6d1575fc6a28b85
floruo <florian.ruosch@uzh.ch>
2025-05-19 14:16:06 +0200

    Implemented clipEmbedding #62

--- src/main/kotlin/org/megras/graphstore/derived/DerivedRelationRegistrar.kt
+++ src/main/kotlin/org/megras/graphstore/derived/DerivedRelationRegistrar.kt
@@ -4,12 +4,14 @@ import org.megras.data.fs.FileSystemObjectStore
 import org.megras.data.graph.QuadValue
 import org.megras.graphstore.QuadSet
 import org.megras.graphstore.derived.handlers.AverageColorHandler
+import org.megras.graphstore.derived.handlers.ClipEmbeddingHandler
 
 class DerivedRelationRegistrar(private val quads: QuadSet, private val objectStore: FileSystemObjectStore) {
     private val handlers = mutableListOf<DerivedRelationHandler<QuadValue>>()
 
     init {
         register(AverageColorHandler(quads, objectStore) as DerivedRelationHandler<QuadValue>)
+        register(ClipEmbeddingHandler(quads, objectStore) as DerivedRelationHandler<QuadValue>)
     }
 
     private fun register(handler: DerivedRelationHandler<QuadValue>) {
---
+++ src/main/kotlin/org/megras/graphstore/derived/handlers/ClipEmbeddingHandler.kt
@@ -0,0 +1,52 @@
+package org.megras.graphstore.derived.handlers
+
+import org.megras.data.fs.FileSystemObjectStore
+import org.megras.data.fs.StoredObjectId
+import org.megras.data.graph.FloatVectorValue
+import org.megras.data.graph.LocalQuadValue
+import org.megras.data.graph.StringValue
+import org.megras.data.graph.URIValue
+import org.megras.data.schema.MeGraS
+import org.megras.graphstore.QuadSet
+import org.megras.graphstore.derived.DerivedRelationHandler
+import org.megras.util.Constants
+import org.megras.util.embeddings.ClipEmbeddings
+
+class ClipEmbeddingHandler(private val quadSet: QuadSet, private val objectStore: FileSystemObjectStore) : DerivedRelationHandler<FloatVectorValue> {
+    override val predicate: URIValue = URIValue("${Constants.DERIVED_PREFIX}/clipEmbedding")
+
+    override fun canDerive(subject: URIValue): Boolean {
+        if (subject !is LocalQuadValue) {
+            return false
+        }
+
+        getPath(subject) ?: return false
+
+        return true
+    }
+
+    private fun getPath(subject: URIValue): String? {
+        val canonicalId = this.quadSet.filter(
+            setOf(subject),
+            setOf(MeGraS.CANONICAL_ID.uri),
+            null
+        ).firstOrNull()?.`object` as? StringValue ?: return null
+
+        val osId = StoredObjectId.of(canonicalId.value) ?: return null
+        return objectStore.storageFile(osId).absolutePath
+    }
+
+    override fun derive(subject: URIValue): Collection<FloatVectorValue> {
+        if (subject !is LocalQuadValue) {
+            return emptyList()
+        }
+
+        val path = getPath(subject) ?: return emptyList()
+
+        val embedding = ClipEmbeddings.getImageEmbedding(path)
+
+        return listOf(
+            FloatVectorValue(embedding)
+        )
+    }
+}
