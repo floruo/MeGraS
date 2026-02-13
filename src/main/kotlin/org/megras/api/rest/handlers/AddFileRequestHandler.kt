f792b93b3799ddc66f65bd0d8cc8c4b72a0bcf62
floruo <flo.ruo@gmail.com>
2026-02-13 17:58:11 +0100

    Added metaSkip option to addFile RESTI API

--- src/main/kotlin/org/megras/api/rest/handlers/AddFileRequestHandler.kt
+++ src/main/kotlin/org/megras/api/rest/handlers/AddFileRequestHandler.kt
@@ -23,6 +23,9 @@ class AddFileRequestHandler(private val quads: MutableQuadSet, private val objec
         operationId = OpenApiOperation.AUTO_GENERATE,
         methods = [HttpMethod.POST],
         requestBody = OpenApiRequestBody([OpenApiContent(type = "multipart/form-data")]),
+        queryParams = [
+            OpenApiParam(name = "metaSkip", type = Boolean::class, description = "Skip metadata extraction from file", required = false)
+        ],
         responses = [
             OpenApiResponse(status = "200", description = "Returns mapping of uploaded filenames to stored object IDs", content = [OpenApiContent(type = "application/json")]),
             OpenApiResponse(status = "400", description = "Invalid request", content = [OpenApiContent(RestErrorStatus::class)])
@@ -36,10 +39,12 @@ class AddFileRequestHandler(private val quads: MutableQuadSet, private val objec
             throw RestErrorStatus(400, "no file")
         }
 
+        val metaSkip = ctx.queryParam("metaSkip")?.toBoolean() ?: false
+
         val mapper = jacksonObjectMapper()
 
         val ids = files.associate { uploadedFile ->
-            val mapEntry = uploadedFile.filename() to FileUtil.addFile(objectStore, quads, PseudoFile(uploadedFile))
+            val mapEntry = uploadedFile.filename() to FileUtil.addFile(objectStore, quads, PseudoFile(uploadedFile), metaSkip)
 
             //check for metadata
             val meta = ctx.formParam(uploadedFile.filename())
